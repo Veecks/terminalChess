9d77d0de11c6dc3933b3c167a38c6a5ba42cda87
Veecks <guilhermelibardi@hotmail.com>
2021-06-16 02:00:50 -0400

    Position class created into boardgame package.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Chess!");
-
+		
+		Position pos = new Position(1,3);
+		System.out.println(pos);
 	}
 
 }
