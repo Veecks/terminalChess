0b469cc4bd4b71de8be75354d5f3b109d81f4dfd
Veecks <guilhermelibardi@hotmail.com>
2021-06-16 03:08:07 -0400

    PrintBoard implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Chess!");
 		
-		Position pos = new Position(1,3);
-		System.out.println(pos);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
